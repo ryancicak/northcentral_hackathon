8baf1f808f6ba914c8dc6598cd36be35567e5de4
Amer Issa <amer.issa1@gmail.com>
2017-10-23 21:01:19 -0400

    adjusted deployer.sh

--- CloudBreakArtifacts/cloudbreak-cmd/deployer.sh
+++ CloudBreakArtifacts/cloudbreak-cmd/deployer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 #Get properties 
 
 if [ -f ./.deploy.config ]; then
@@ -28,29 +28,29 @@ cloudbreak="java -jar CloudBreakArtifacts/cloudbreak-cmd/cloudbreak-shell.jar --
 
 
 #Check if Cluster exists
-echo stack list > file 
-$cloudbreak --cmdfile=file | grep $CloudBreakClusterName || { echo "Cluster Already Exists"; exit 1; }
+echo stack show --name $CloudBreakClusterName > file 
+$cloudbreak --cmdfile=file | grep -q FAILED || { echo "Cluster Already Exists"; exit 1; }
 #Check if Credentials exists
-echo credential list > file
-$cloudbreak --cmdfile=file | grep $CloudBreakCredentials || { echo "Credential does not exist"; exit 1; }
-echo credential select $CloudBreakCredentials > deploy
+echo credential show --name $CloudBreakCredentials  > file
+$cloudbreak --cmdfile=file  | grep -q FAILED &&  { echo "Credential does not exist"; exit 1; }
+echo credential select --name $CloudBreakCredentials > deploy
 #Check if Template is avaliable
-echo template list > file
-$cloudbreak --cmdfile=file | grep $CloudBreakTemplate || { echo "Template does not exist"; exit 1; }
+echo template show --name $CloudBreakTemplate > file
+$cloudbreak --cmdfile=file | grep -q FAILED &&  { echo "Template does not exist"; exit 1; }
 
 #Check if Security Group exists
-echo securitygroup list > file 
-$cloudbreak --cmdfile=file | grep $CloudBreakSecurityGroup || { echo "SecurityGroup does not exist"; exit 1; }
+echo securitygroup show --name $CloudBreakSecurityGroup  > file 
+$cloudbreak --cmdfile=file | grep -q FAILED &&  { echo "SecurityGroup does not exist"; exit 1; }
 
 #Check if network Exists:
-echo network list > file
-$cloudbreak --cmdfile=file | grep $CloudBreakNetwork || { echo "Network does not exist"; exit 1; }
-echo network select $CloudBreakNetwork >> deploy
+echo network show --name $CloudBreakNetwork > file
+$cloudbreak --cmdfile=file | grep -q FAILED &&  { echo "Network does not exist"; exit 1; }
+echo network select --name  $CloudBreakNetwork >> deploy
 
 
 #Check if blueprint exists
-echo blueprint list > file
-if $cloudbreak --cmdfile=file | grep -q alarmfatigue-$Version; then 
+echo blueprint show --name alarmfatigue-$Version > file
+if $cloudbreak --cmdfile=file | grep -q SUCCESS; then 
     echo "Blueprint Version is already deployed. Going to use it"
     echo blueprint select --name alarmfatigue-$Version >> deploy
 else
@@ -61,32 +61,32 @@ else
 fi 
 
 #Check if Recipes are there
-echo recipe list > file
-if $cloudbreak --cmdfile=file | grep -q alaramfatigue-post-$Version; then
+echo recipe show --name alaramfatigue-post-$(echo $Version | sed 's/\./-/g') > file
+if $cloudbreak --cmdfile=file | grep -q SUCCESS; then
     echo "Post Install Recipe is Loaded. Going to add it"
 else
     echo "Deploying Post Install Recipe"
-    echo recipe create --name alaramfatigue-post-$Version --type POST --scriptFile CloudBreakArtifacts/recipes/alarmfatigue-demo-post-install.sh > file
+    echo recipe create --name alaramfatigue-post-$(echo $Version | sed 's/\./-/g') --type POST --scriptFile CloudBreakArtifacts/recipes/alarmfatigue-demo-post-install.sh > file
     $cloudbreak --cmdfile=file
 fi
 
-if $cloudbreak --cmdfile=file | grep -q alaramfatigue-sam-$Version; then
+echo recipe show --name alaramfatigue-sam-$(echo $Version | sed 's/\./-/g') > file
+if $cloudbreak --cmdfile=file | grep -q SUCCESS; then
     echo "SAM Install Recipe is Loaded. Going to add it"
 else
     echo "Deploying SAM Install Recipe"
-    echo recipe create --name alaramfatigue-sam-$Version --type POST --scriptFile CloudBreakArtifacts/recipes/alarmfatigue-demo-sam-install.sh > file
+    echo recipe create --name alaramfatigue-sam-$(echo $Version | sed 's/\./-/g') --type POST --scriptFile CloudBreakArtifacts/recipes/alarmfatigue-demo-sam-install.sh > file
     $cloudbreak --cmdfile=file
 fi
 
 
 #Setup instance groups and cluster commands
 
-echo instancegroup configure --OPENSTACK --instanceGroup host_group_1  --nodecount 1 --ambariServer true --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
-echo instancegroup configure --OPENSTACK --instanceGroup host_group_2  --nodecount 1 --ambariServer false --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
-echo instancegroup configure --OPENSTACK --instanceGroup host_group_3  --nodecount 1 --ambariServer false --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
-echo hostgroup configure --hostgroup host_group_1 --recipeNames alaramfatigue-post-$Version,alaramfatigue-sam-$Version >> deploy
+echo instancegroup configure --instanceGroup host_group_1  --nodecount 1 --ambariServer true --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
+echo instancegroup configure --instanceGroup host_group_2  --nodecount 1 --ambariServer false --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
+echo instancegroup configure --instanceGroup host_group_3  --nodecount 1 --ambariServer false --templateName  $CloudBreakTemplate --securityGroupName $CloudBreakSecurityGroup >> deploy
+echo hostgroup configure --hostgroup host_group_1 --recipeNames alaramfatigue-post-$(echo $Version | sed 's/\./-/g'),alaramfatigue-sam-$(echo $Version | sed 's/\./-/g') >> deploy
 echo stack create --OPENSTACK --name $CloudBreakClusterName --region $CloudBreakRegion --availabilityZone $CloudBreakAvailabilityZone >> deploy
-echo cluster create --description "Alarm Fatigue $Version" >> deploy 
 
 
 # Create cluster
